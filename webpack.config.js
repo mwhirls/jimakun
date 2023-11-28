5b1fb766a0f72bac365f0dbb82f7f3e76def6279
Megan Worley <megan.worley@live.com>
2023-11-28 13:38:06 -0800

    Set up PostCSS loaders and Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,8 @@ module.exports = function (_env, argv) {
                     test: /\.css$/,
                     use: [
                         isProduction ? MiniCssExtractPlugin.loader : "style-loader",
-                        "css-loader"
+                        "css-loader",
+                        "postcss-loader"
                     ]
                 },
                 {
