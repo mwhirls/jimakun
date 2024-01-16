eff6ebf53a579b58f19207073a53cc73a517af9f
mwhirls <megan.worley@live.com>
2024-01-16 09:53:12 -0800

    Better layout of page buttons

--- src/content-scripts/components/lookup/Pagination.tsx
+++ src/content-scripts/components/lookup/Pagination.tsx
@@ -44,7 +44,7 @@ function pageButton(content: Icon | string, selected: Selected, mirrored: Mirror
     const contentElem = buttonContent(content);
     const color = selected === Selected.Yes ? "bg-red-500" : "bg-white";
     const textColor = selected === Selected.Yes ? "text-white" : "text-black";
-    const mirror = mirrored === Mirrored.Yes ? "-scale-x-1" : "scale-x-1";
+    const mirror = mirrored === Mirrored.Yes ? "-scale-x-100" : "scale-x-100";
     return (
         <button onClick={() => onClick()} className={`border border-solid rounded-lg border-slate-300 ${color} ${textColor} ${mirror}`}>
             {contentElem}
@@ -54,7 +54,6 @@ function pageButton(content: Icon | string, selected: Selected, mirrored: Mirror
 
 function Pagination({ numPages, onPageClicked }: PaginationProps) {
     const [selectedPage, setSelectedPage] = useState<number>(0);
-    const pages = Array.from(Array(numPages).keys());
     const firstIcon = new Icon(FIRST_ICON);
     const prevIcon = new Icon(PREV_ICON);
 
@@ -68,19 +67,27 @@ function Pagination({ numPages, onPageClicked }: PaginationProps) {
     const onClickNext = () => onClick(selectedPage + 1);
     const onClickLast = () => onClick(numPages - 1);
 
+    const pages = Array.from(Array(numPages).keys());
+    const firstFewPages = pages.slice(0, Math.min(3, pages.length - 2));
+    const finalPages = pages.slice(pages.length - 1);
+
+    const numberedPage = (page: number) => {
+        const selected = page === selectedPage ? Selected.Yes : Selected.No;
+        const onClickNumbered = () => onPageClicked(page);
+        return (
+            <div key={page}>
+                {pageButton(page.toString(), selected, Mirrored.No, onClickNumbered)}
+            </div>
+        );
+    }
+
     return (
-        <div>
+        <div className="flex flex-auto flex-row flex-nowrap basis-16 gap-4">
             {pageButton(firstIcon, Selected.No, Mirrored.No, onClickFirst)}
             {pageButton(prevIcon, Selected.No, Mirrored.No, onClickPrev)}
-            {pages.map((page) => {
-                const selected = page === selectedPage ? Selected.Yes : Selected.No;
-                const onClickNumbered = () => onPageClicked(page);
-                return (
-                    <div key={page}>
-                        {pageButton(page.toString(), selected, Mirrored.No, onClickNumbered)}
-                    </div>
-                );
-            })}
+            {firstFewPages.map((page) => numberedPage(page))}
+            {pages.length < 5 || <div>...</div>}
+            {finalPages.map((page) => numberedPage(page))}
             {pageButton(prevIcon, Selected.No, Mirrored.Yes, onClickNext)}
             {pageButton(firstIcon, Selected.No, Mirrored.Yes, onClickLast)}
         </div>
