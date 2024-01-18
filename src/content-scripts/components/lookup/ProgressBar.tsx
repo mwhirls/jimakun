668faefdc1deec9dd5fec37844dc023320e7b767
mwhirls <megan.worley@live.com>
2024-01-18 12:05:32 -0800

    Add shimmer effect to progress bar

--- src/content-scripts/components/lookup/ProgressBar.tsx
+++ src/content-scripts/components/lookup/ProgressBar.tsx
@@ -19,7 +19,7 @@ function ProgressBar({ id, label, maxValue, units }: ProgressBarProps) {
     const safeValue = Math.min(value, maxValue);
     const complete = safeValue >= maxValue;
     const progressText = complete ? 'Complete!' : `In progress | ${safeValue} / ${maxValue} ${units}`;
-    const color = complete ? 'bg-green-600' : 'bg-blue-600';
+    const bgColor = complete ? 'bg-green-600' : 'bg-blue-600';
     const valueStyle = {
         width: `${Math.min(100, safeValue / maxValue * 100)}%`
     };
@@ -27,8 +27,8 @@ function ProgressBar({ id, label, maxValue, units }: ProgressBarProps) {
     return (
         <div className="leading-tight">
             <label htmlFor={id} className='font-normal text-3xl'>{label}</label>
-            <div className="rounded-full bg-slate-300 h-6" onClick={() => onClick(10)}>
-                <div style={valueStyle} className={`${color} transition-all rounded-full h-full`}></div>
+            <div className="rounded-full bg-slate-300 h-4 overflow-hidden" onClick={() => onClick(10)}>
+                <div style={valueStyle} className={`transition-all ${bgColor} rounded-full h-full shimmer`}></div>
             </div>
             <span className="text-slate-400 text-2xl font-light">{progressText}</span>
         </div>
