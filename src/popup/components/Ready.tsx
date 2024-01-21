740c196fcce7550975e5170231d8fa2df95fe6d6
mwhirls <megan.worley@live.com>
2024-01-20 21:10:45 -0800

    Tweak colors on Ready screen

--- src/popup/components/Ready.tsx
+++ src/popup/components/Ready.tsx
@@ -5,9 +5,9 @@ function DatabaseReady() {
     return (
         <div className='flex flex-col justify-center items-center m-auto gap-4 w-[26rem] max-w-full max-h-full p-8 mt-8'>
             <div>
-                <div className='font-bold text-4xl mb-4 text-center text-green-500'>Ready!</div>
+                <div className='font-bold text-4xl mb-4 text-center'>Ready!</div>
                 <div className='w-4/5 mx-auto'>
-                    <CheckContained></CheckContained>
+                    <CheckContained className='w-full h-full [&>path]:stroke-green-500'></CheckContained>
                 </div>
             </div>
             <div className='w-4/5 text-2xl font-light text-center text-slate-400 w-11/12'>Jimakun has successfully updated. You may need to refresh Netflix for changes to take effect.</div>
