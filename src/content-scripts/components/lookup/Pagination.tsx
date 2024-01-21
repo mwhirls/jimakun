ad4670650e721728aa3b5beccc26618a6e75a6ce
mwhirls <megan.worley@live.com>
2024-01-21 09:57:38 -0800

    Clean up PageItem rendering logic

--- src/content-scripts/components/lookup/Pagination.tsx
+++ src/content-scripts/components/lookup/Pagination.tsx
@@ -1,14 +1,6 @@
 import React, { useState } from "react";
 import ChevronLeftIcon from '../../../../public/assets/chevron-left.svg';
 
-class SVG {
-    jsx: JSX.Element;
-
-    constructor(jsx: JSX.Element) {
-        this.jsx = jsx;
-    }
-}
-
 enum Selected {
     Yes,
     No,
@@ -19,47 +11,55 @@ enum Mirrored {
     No,
 }
 
-function itemContent(content: SVG | string) {
-    const inner = () => {
-        if (content instanceof SVG) {
-            return (
-                <div className="block m-auto">
-                    {content.jsx}
-                </div>
-            )
-        } else if (typeof content === 'string') {
-            return (
-                <div className="text-2xl font-normal text-center mx-3">{content}</div>
-            )
-        } else {
-            throw new Error('unrecognized content type');
-        }
+class SVG {
+    content: JSX.Element;
+
+    constructor(content: JSX.Element) {
+        this.content = content;
     }
-    return (
-        <div>
-            {inner()}
-        </div>
-    )
+
+    render() {
+        return (
+            <div className="block m-auto">
+                {this.content}
+            </div>
+        )
+    }
+}
+
+class Text {
+    content: string;
+
+    constructor(content: string) {
+        this.content = content;
+    }
+
+    render() {
+        return (
+            <div className="text-2xl font-normal text-center mx-3">{this.content}</div>
+        )
+    }
 }
 
 class PageItem {
-    content: SVG | string;
+    content: SVG | Text;
 
-    constructor(content: SVG | string) {
+    constructor(content: SVG | Text) {
         this.content = content;
     }
 
     render(): JSX.Element {
-        return itemContent(this.content);
+        return this.content.render();
     }
 }
+
 
 class PageNavigation extends PageItem {
     onClick: () => void;
     selected: Selected;
     mirrored: Mirrored;
 
-    constructor(content: SVG | string, onClick: () => void, selected: Selected, mirrored: Mirrored) {
+    constructor(content: SVG | Text, onClick: () => void, selected: Selected, mirrored: Mirrored) {
         super(content);
         this.onClick = onClick;
         this.selected = selected;
@@ -71,7 +71,7 @@ class PageNavigation extends PageItem {
         const border = this.selected === Selected.Yes ? "border border-solid border-black" : "";
         return (
             <button onClick={() => this.onClick()} className={`w-full h-full align-top ${border} rounded-lg bg-white hover:bg-red-500 active:bg-red-400 text-black hover:text-white ${mirror}`}>
-                {itemContent(this.content)}
+                {this.content.render()}
             </button>
         )
     }
@@ -80,7 +80,8 @@ class PageNavigation extends PageItem {
 function numberedPage(page: number, selectedPage: number, onPageClicked: (page: number) => void) {
     const selected = page === selectedPage ? Selected.Yes : Selected.No;
     const onClickNumbered = () => onPageClicked(page);
-    return new PageNavigation((page + 1).toString(), onClickNumbered, selected, Mirrored.No);
+    const number = new Text((page + 1).toString());
+    return new PageNavigation(number, onClickNumbered, selected, Mirrored.No);
 }
 
 export interface PaginationProps {
@@ -106,7 +107,7 @@ function Pagination({ numPages, onPageClicked }: PaginationProps) {
 
     const pages = Array.from(Array(numPages).keys());
     const firstFewPages = pages.slice(0, Math.min(2, pages.length - 2)).map(page => numberedPage(page, selectedPage, onClick));
-    const collapsedPages = pages.length >= 5 ? new PageItem("...") : undefined;
+    const collapsedPages = pages.length >= 5 ? new PageItem(new Text("...")) : undefined;
     const finalPages = pages.slice(pages.length - 1).map(page => numberedPage(page, selectedPage, onClick));
     const items = [
         new PageNavigation(prevIcon, onClickPrev, Selected.No, Mirrored.No),
