4b3bcb67a8d41d103b7cdb91e33c247d540acb09
mwhirls <megan.worley@live.com>
2024-01-22 13:19:06 -0800

    Options page UI tweaks

--- src/options/components/Header.tsx
+++ src/options/components/Header.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 function Header() {
     return (
-        <div className="border border-b border-solid border-slate-200">
-            <div className="flex flex-row items-center gap-4 ml-8 my-8">
-                <img src='icons/icon32.png'></img>
-                <h2 className="text-4xl font-bold">Jimakun</h2>
+        <div className="border-b border-solid border-slate-200">
+            <div className="ml-[10%] my-8">
+                <img className="inline" src='icons/icon32.png'></img>
+                <h2 className="inline ml-4 text-3xl font-bold align-middle">Jimakun</h2>
             </div>
         </div>
     )
--- src/options/components/Settings.tsx
+++ src/options/components/Settings.tsx
@@ -5,8 +5,8 @@ function Settings() {
     return (
         <>
             <Header></Header>
-            <div className="w-4/5 mx-auto my-8 flex flex-col gap-4">
-                <h1 className="text-6xl font-bold">Settings</h1>
+            <div className="w-3/5 mx-auto my-8 flex flex-col gap-4">
+                <h1 className="text-5xl font-bold">Settings</h1>
                 <hr></hr>
                 <div className="grid grid-cols-2 gap-8 divide-y mt-8 items-center">
                     <div>
@@ -14,7 +14,7 @@ function Settings() {
                         <p className="text-2xl text-slate-400 mt-2">Forcefully purge all dictionary databases and attempt to reimport dictionaries. This can be used to reset the state of the backend in the event that Jimakun encounters an error during upgrade.</p>
                     </div>
                     <div className="justify-self-end">
-                        <button className="text-red-600 text-3xl font-bold p-4 bg-slate-100 border border-solid border-slate-400 rounded-md hover:bg-red-600 hover:text-white">Purge</button>
+                        <button className="text-red-600 text-3xl font-bold py-4 px-8 bg-slate-100 border border-solid border-slate-400 rounded-md hover:bg-red-600 hover:text-white">Purge</button>
                     </div>
                 </div>
             </div>
