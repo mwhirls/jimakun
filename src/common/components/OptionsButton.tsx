a71a7f5d1f1f1a300c5053270abaa40dc9c4d741
mwhirls <megan.worley@live.com>
2024-01-22 18:36:41 -0800

    Open options page

--- src/common/components/OptionsButton.tsx
+++ src/common/components/OptionsButton.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { Cog8ToothIcon } from '@heroicons/react/24/outline'
 
+function openOptions() {
+    chrome.runtime.openOptionsPage(() => {
+        const lastError = chrome.runtime.lastError;
+        if (lastError) {
+            console.error(lastError);
+        }
+    });
+}
+
 function OptionsButton() {
     return (
-        <button className="text-black text-2xl font-medium p-4 bg-white rounded-lg border border-solid border-gray-200 hover:border-gray-300 hover:drop-shadow-md active:bg-gray-200">
+        <button className="text-black text-2xl font-medium p-4 bg-white rounded-lg border border-solid border-gray-200 hover:border-gray-300 hover:drop-shadow-md active:bg-gray-200" onClick={() => openOptions()}>
             <span className="w-10 inline-block align-middle mr-4">
                 <Cog8ToothIcon className="text-slate-400"></Cog8ToothIcon>
             </span>
