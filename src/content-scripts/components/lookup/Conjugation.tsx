89a1667910cad4d86f27c729632a2da823dd3206
mwhirls <megan.worley@live.com>
2024-01-31 10:43:29 -0800

    Handle more conjugations

--- src/content-scripts/components/lookup/Conjugation.tsx
+++ src/content-scripts/components/lookup/Conjugation.tsx
@@ -5,13 +5,19 @@ import { ArrowLongRightIcon } from '@heroicons/react/24/outline'
 enum TokenType {
     Ageru,
     DictionaryForm,
+    Conditional,
     Continuative,
     Imperative,
     Kureru,
     Kuru,
+    Masu,
     Morau,
+    NaiForm,
     Passive,
+    PassivePotential,
     PastTense,
+    Suru,
+    Tara,
     TeForm,
     Tsudukeru,
     Unknown,
@@ -28,6 +34,8 @@ function getInfoText(inflection: TokenType) {
     switch (inflection) {
         case TokenType.DictionaryForm:
             return 'dictionary form';
+        case TokenType.Conditional:
+            return 'ば conditional form';
         case TokenType.TeForm:
             return 'て form';
         case TokenType.PastTense:
@@ -40,14 +48,24 @@ function getInfoText(inflection: TokenType) {
             return "くれる auxiliary verb - to do for one; to take the trouble to do​; to do to someone's disadvantage";
         case TokenType.Ageru:
             return "あげる auxiliary verb - to do for (the sake of someone else​)";
+        case TokenType.Masu:
+            return 'polite form';
         case TokenType.Morau:
             return "もらう auxillary verb - to get someone to do something​";
+        case TokenType.NaiForm:
+            return 'negative form';
         case TokenType.Passive:
             return 'indicates passive voice (incl. the "suffering passive")';
+        case TokenType.PassivePotential:
+            return '1. indicates passive voice (incl. the "suffering passive"); 2. indicates the potential form'
+        case TokenType.Suru:
+            return 'dictionary form (suru-verb)';
+        case TokenType.Tara:
+            return 'たら conditional form';
         case TokenType.Tsudukeru:
             return '続ける auxillary verb - to continue; to keep up; to keep on';
     }
-    return 'unknown token'
+    return 'unknown form'
 }
 
 function getTokenType(token: bunsetsu.Token) {
@@ -56,17 +74,36 @@ function getTokenType(token: bunsetsu.Token) {
             return TokenType.TeForm;
         case 'くる':
             return TokenType.Kuru;
-        case 'た':
-        case 'かった':
+        case 'た': {
+            if (token.surfaceForm === 'たら') {
+                return TokenType.Tara;
+            }
             return TokenType.PastTense;
+        }
+        case 'かった': {
+            if (token.surfaceForm === 'かったら') {
+                return TokenType.Tara;
+            }
+            return TokenType.PastTense;
+        }
         case 'くれる':
             return TokenType.Kureru;
         case 'あげる':
             return TokenType.Ageru;
         case 'もらう':
             return TokenType.Morau;
+        case 'られる':
+            return TokenType.PassivePotential;
         case 'れる':
             return TokenType.Passive;
+        case 'ます':
+            return TokenType.Masu;
+        case 'ない':
+            return TokenType.NaiForm;
+        case 'ば':
+            return TokenType.Conditional;
+        case 'する':
+            return TokenType.Suru;
     }
     return TokenType.Unknown;
 }
@@ -198,7 +235,7 @@ function InfoText({ forms, selectedIndex }: InfoTextProps) {
     return (
         <div className='leading-none my-4'>
             <span className='inline-block text-3xl text-black font-normal'>{label}</span>
-            <span className='ml-4 text-2xl text-slate-400 font-normal'>{infoText}</span>
+            <span className='ml-4 text-3xl text-slate-400 font-normal'>{infoText}</span>
         </div>
     )
 }
