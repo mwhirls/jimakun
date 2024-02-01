0877bc259ce7707b0d73515469e227ba8e987a56
mwhirls <megan.worley@live.com>
2024-02-01 10:05:10 -0800

    Add event handler to toggle switch

--- src/common/components/Toggle.tsx
+++ src/common/components/Toggle.tsx
@@ -2,14 +2,21 @@ import React from "react";
 import './Toggle.css'
 
 interface ToggleProps {
+    toggled: boolean;
+    onToggled: (toggled: boolean) => void;
     className?: string;
 }
 
-function Toggle({ className }: ToggleProps) {
+function Toggle({ className, toggled, onToggled }: ToggleProps) {
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const checked = e.currentTarget.checked;
+        onToggled(checked);
+    };
+
     return (
         <div className={className}>
             <label className="relative inline-flex cursor-pointer items-center p-1">
-                <input checked type="checkbox" className="peer sr-only" />
+                <input type="checkbox" className="peer sr-only" checked={toggled} onChange={onChange} />
                 <span className="h-6 w-11 rounded-full bg-gray-300 leading-5 shadow-inner peer-checked:bg-blue-600 peer-focus:ring-4">
                 </span>
                 <span className="absolute h-5 w-5 rounded-full ml-[0.1rem] inline-block bg-white align-middle leading-8 drop-shadow-sm peer-checked:translate-x-full transition ease-in-out"></span>
